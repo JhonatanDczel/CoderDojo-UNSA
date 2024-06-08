fceda264a2745cc2ae6d221933259bc4891bcd4d
JhonatanDczel <jariasq@unsa.edu.pe>
2024-06-08 16:20:33 -0500

    Estructura inicial

--- groups/apps.py
+++ groups/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AssignmentsConfig(AppConfig):
+class GroupsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'assignments'
+    name = 'groups'
